f6090b59d982ad48e999f6fbfc7bd4ba6ccd3486
AntonAntipov29 <antipovhuman@gmail.com>
2022-08-10 16:05:55 +0300

    Добавьте файлы проекта.

---
+++ Конвертер валют/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Конвертер_валют
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double USD;
+            double dollar = 37.5;
+
+
+            Console.WriteLine("Введите сумму в долларах");
+            USD = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Сумма в гривне = " + USD * dollar);
+
+
+        }
+    }
+}
