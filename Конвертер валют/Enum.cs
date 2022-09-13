02e50446b3df4c516af6abd50adc6a9527c18dd3
AntonAntipov29 <antipovhuman@gmail.com>
2022-09-13 12:44:08 +0300

    New place for Enum

---
+++ Конвертер валют/Enum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Конвертер_валют
+{
+    internal class Enum
+    {
+       public enum TypeOfUserInput { command, currency, money, year, number};
+    }
+}
