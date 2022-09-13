9156e26c5cc6508a76c6685d83cfdfb4b9b2aedb
AntonAntipov29 <antipovhuman@gmail.com>
2022-09-13 12:45:31 +0300

    Empty Age Calculator added

---
+++ Конвертер валют/AgeCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Конвертер_валют
+{
+    internal class AgeCalculator
+    {
+        public void AgeCalculatorBody()
+        {
+            Console.WriteLine("Данна функцiя буде додана згодом, очiкуйте на оновлення.");
+            Console.WriteLine("Щоб повернутись в попередньє меню введiть Return.");
+            Console.WriteLine("Щоб закрити додаток введiть Exit.");
+            string answer = Console.ReadLine();
+            if (answer == TaxCalculator.ExitIndex)
+            {
+                Environment.Exit(0);
+            }
+            else if (answer == TaxCalculator.ReturnIndex)
+            {
+                Console.Clear();
+                MainMenu.Main();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Неправильний ввiд! Використовуйте Exit або Calculate again.");
+                AgeCalculatorBody();
+            }
+        }
+    }
+}
