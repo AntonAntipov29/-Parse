512c5f4b1bedfc91968caae6b9f1925355f44474
AntonAntipov29 <antipovhuman@gmail.com>
2022-09-16 23:21:03 +0300

    Improved Tax Calculator

--- Конвертер валют/TaxCalculator.cs
+++ Конвертер валют/TaxCalculator.cs
@@ -5,90 +5,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Конвертер_валют
+namespace Calculator_program
 {
     
-    public class TaxCalculator
+    class TaxCalculator
     {
+         
         public const string ExitIndex = "Exit";
         public const string CalcAgainIndex = "Calculate again";
         public const string ReturnIndex = "Return";
         public const string dollarIndex = "USD";
         public const string euroIndex = "EUR";
         public const string hryvniaIndex = "UAH";
+        double yearProfit;
+        public string currency;
+        public double profit;
+        public double singleTaxRate = 0.05;
+        public double socialContRate = 0.22;
+        public const double MIN_SALARY = 6500;
+        public double singleTax;
+        public double socialCont;
+        public double dollarRate = 39.77;
+        public double euroRate = 39.4;
+        public double hryvniaRate = 1;
+        public double currencyVariant = 0;
+        public string formatMoney = "{0:N}";
+        public string finalAnswer;
 
-        public void TaxCalculatorBody()
+        public void Main()
         {
-            double yearProfit;
-            string currency;
-            double profit;
-            float singleTaxRate = 0.05f;
-            float socialContRate = 0.22f;
-            const float MIN_SALARY = 6500;
-            double singleTax;
-            double socialCont;
-            float dollarRate = 39.77f;
-            float euroRate = 39.4f;
-            float hryvniaRate = 1f;
-            float currencyVariant = 0;
-            string formatMoney = "{0:N}";
-
+            
+           GettingInput();
+           Calculation();
+           ShowResult();
+           
+        }
+     
+        private void GettingInput()
+        {
 
             Console.WriteLine("Введiть валюту в якiй ви отримуєте дохiд UAH, USD або EUR");
-            currency = UserInput.GetUserInput(Enum.TypeOfUserInput.currency);
-            Console.WriteLine($"Введена валюта - {currency}. ");
+            UserInput userInput = new UserInput();
+            currency = userInput.GetUserInput(TypeOfUserInput.currency);
+            Console.WriteLine($"Введена валюта - {currency}.");
             Console.WriteLine("Натиснiть ENTER, щоб продовжити.");
-            Console.ReadKey();
-            yearProfit = YearProfit();
-            Console.Write("Ви ввели " + formatMoney, yearProfit);
-            Console.Write(" " + currency);
-            Console.WriteLine(". Для розрахунку податкiв натиснiть ENTER");
-            Console.ReadKey();
-            Console.Clear();
-
-            //maths operations with taxes 
-            if (currency == hryvniaIndex)
-            {
-                currencyVariant = hryvniaRate;
-            }
-            else if (currency == dollarIndex)
-            {
-                currencyVariant = dollarRate;
-            }
-            else if (currency == euroIndex)
-            {
-                currencyVariant = euroRate;
-            }
-
-
-            yearProfit = yearProfit * currencyVariant;
-            singleTax = yearProfit * singleTaxRate;
-            socialCont = MIN_SALARY * socialContRate;
-            profit = yearProfit - singleTax - socialCont;
-
-            //output results in console
-
-
-            Console.Write("Дохiд до вирахування податкiв - " + formatMoney, yearProfit);
-            Console.WriteLine(" гривень");
-            Console.Write("Всього єдиного податку (5%) - " + formatMoney, singleTax);
-            Console.WriteLine(" гривень");
-            Console.Write("Всього єдиного соцiального внеску (22%) - " + formatMoney, socialCont);
-            Console.WriteLine(" гривень");
-            Console.Write("Прибуток пiсля вирахування податкiв - " + formatMoney, profit);
-            Console.WriteLine(" гривень");
-
-            FinalAnswer();
+    
         }
 
-        double YearProfit()
-        {
+        void Calculation()
+        { 
+
             string[] months = { "Сiчень", "Лютий", "Березень", "Квiтень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень" };
             string[] monthsProfit = new string[12];
             double[] monthsProfitDouble = new double[12];
             int count = 0;
             double result = 0;
-
 
             NumberFormatInfo formatComa = new NumberFormatInfo()
             {
@@ -103,7 +74,8 @@ namespace Конвертер_валют
             {
                 Console.Clear();
                 Console.WriteLine("Введiть дохiд за " + months[count]);
-                monthsProfit[count] = UserInput.GetUserInput(Enum.TypeOfUserInput.money);
+                UserInput userInputMoney = new UserInput();
+                monthsProfit[count] = userInputMoney.GetUserInput(TypeOfUserInput.money);
 
                 if (monthsProfit[count].Contains(","))
                 {
@@ -120,16 +92,51 @@ namespace Конвертер_валют
                 result += monthsProfitDouble[count];
                 count++;
             }
-            return result;
+            
+            if (currency == hryvniaIndex)
+            {
+                currencyVariant = hryvniaRate;
+            }
+            else if (currency == euroIndex)
+            {
+                currencyVariant = euroRate;
+            }
+            else if (currency == dollarIndex)
+            {
+                currencyVariant = dollarRate;
+            }
+           
+            yearProfit = result;
+            yearProfit = yearProfit * currencyVariant;
+            singleTax = yearProfit * singleTaxRate;
+            socialCont = MIN_SALARY * socialContRate;
+            profit = yearProfit - singleTax - socialCont;       
+          
+            Console.Clear();
+            Console.Write("Ви ввели " + formatMoney, result);
+            Console.Write(" " + currency);
+            Console.WriteLine(". Для розрахунку податкiв натиснiть ENTER");
+            Console.ReadKey();
+                    
         }
 
-        void FinalAnswer()
+        void ShowResult()
         {
-            string finalAnswer = "";
-
-            Console.WriteLine("Щоб закрити програму напишiть Exit, щоб рахувати знову напишiть Calculate again.");
-            finalAnswer = UserInput.GetUserInput(Enum.TypeOfUserInput.command);
-
+            
+            Console.Clear();
+            Console.Write("Дохiд до вирахування податкiв - " + formatMoney, yearProfit);
+            Console.WriteLine(" гривень");
+            Console.Write("Всього єдиного податку (5%) - " + formatMoney, singleTax);
+            Console.WriteLine(" гривень");
+            Console.Write("Всього єдиного соцiального внеску (22%) - " + formatMoney, socialCont);
+            Console.WriteLine(" гривень");
+            Console.Write("Прибуток пiсля вирахування податкiв - " + formatMoney, profit);
+            Console.WriteLine(" гривень");
+            Console.WriteLine(" ");
+            Console.WriteLine("Щоб закрити програму напишiть Exit, щоб повернутись в головне меню напишiть Return.");
+            Console.WriteLine("Щоб рахувати знову напишiть Calculate again.");
+            UserInput userInputCommand = new UserInput();
+            finalAnswer = userInputCommand.GetUserInput(TypeOfUserInput.command);
             if (finalAnswer == ExitIndex)
             {
                 Environment.Exit(0);
@@ -138,16 +145,23 @@ namespace Конвертер_валют
             {
                 Console.Clear();
                 var reload = new TaxCalculator();
-                reload.TaxCalculatorBody();
+                reload.GettingInput();
+            }
+            else if (finalAnswer == ReturnIndex)
+            {
+                Console.Clear();
+                MainMenu mainMenu = new MainMenu();
+                mainMenu.ShowMenu();
             }
             else
             {
                 Console.Clear();
-                Console.WriteLine("Неправильний ввiд! Використовуйте Exit або Calculate again.");
-                FinalAnswer();
+                Console.WriteLine("Неправильний ввiд! Використовуйте Exit, Return або Calculate again.");
+                ShowResult();
+                userInputCommand.GetUserInput(TypeOfUserInput.command);
             }
-
-            Console.ReadKey();
-        }
+        }  
     }
-}
+} 
+
+
