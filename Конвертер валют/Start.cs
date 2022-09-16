33f38185f293d0900281673065bff075a781a484
AntonAntipov29 <antipovhuman@gmail.com>
2022-09-16 23:19:32 +0300

    Create class Start

---
+++ Конвертер валют/Start.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_program
+{
+    public class Start
+    {
+        static void Main()
+        { 
+            Greeting();
+
+            void Greeting()
+            {
+
+            
+            Console.WriteLine("Вiтеємо Вас в унiверсальному калькуляторi!");
+            Console.WriteLine("Натиснiть будь-яку кнопку, щоб продовжити.");
+            Console.ReadKey();
+            Console.Clear();
+            AgeControl ageControl = new AgeControl();
+            ageControl.Realization();
+            }
+
+        }
+    }
+}
