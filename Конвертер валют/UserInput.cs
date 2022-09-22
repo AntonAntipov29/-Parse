92dc17e36cd02087adab82273aa8ef7abdd91ce1
AntonAntipov29 <antipovhuman@gmail.com>
2022-09-22 21:50:17 +0300

    View of User Input was improver

--- Конвертер валют/UserInput.cs
+++ Конвертер валют/UserInput.cs
@@ -9,103 +9,39 @@ namespace Calculator_program
 {
     public class UserInput
     {
-           public string checkedInput;
-           private string currentInput;
+        public string checkedInput;
+        public string currentInput;
 
-       
+
         //public string GetUserInput(TypeOfUserInput firstType,TypeOfUserInput secondType)
         //{
-            
+
         //} 
 
         public string GetUserInput(TypeOfUserInput type)
         {
-           
+
             currentInput = Console.ReadLine();
 
             if (type == TypeOfUserInput.year)
             {
-                if (int.TryParse(currentInput, out int number))
-                {
-                    checkedInput = currentInput;
-                }
-                else
-                {
-                    ShowWarning();
-                    GetUserInput(TypeOfUserInput.year);
-
-                }
+                TypeOfUserInputYear();
             }
             else if (type == TypeOfUserInput.number)
             {
-               
-                if (int.TryParse(currentInput, out int number))
-                {
-                    checkedInput = currentInput;
-                }
-                else
-                {
-                    Console.Clear();
-                }
+                TypeOfUserInputNumber();
             }
             else if (type == TypeOfUserInput.money)
             {
-
-                double number;
-                
-                bool isLetter = currentInput.All(Char.IsLetter);
-                if (double.TryParse(currentInput, out number))
-                {
-                    checkedInput = currentInput;
-                }
-                else if (isLetter == false && currentInput.Contains("."))
-                {
-                    checkedInput = currentInput;
-                }
-                else
-                {
-                    ShowWarning();
-                    GetUserInput(TypeOfUserInput.money);
-                }
+                TypeOfUserInputMoney();
             }
             else if (type == TypeOfUserInput.currency)
             {
-                if (int.TryParse(currentInput, out int number))
-                {
-                    ShowWarning();
-                    GetUserInput(TypeOfUserInput.currency);
-                }
-                else
-                {
-                    if (currentInput == TaxCalculator.hryvniaIndex || currentInput == TaxCalculator.dollarIndex || currentInput == TaxCalculator.euroIndex)
-                    {
-                        checkedInput = currentInput;
-                    }
-                    else
-                    {
-                        ShowWarning();
-                        GetUserInput(TypeOfUserInput.currency);
-                        
-                    }
-
-                }
-
+                TypeOfUserInputCurrency();
             }
             else if (type == TypeOfUserInput.command)
             {
-              
-
-                if (currentInput == TaxCalculator.calcAgainIndex || currentInput == TaxCalculator.exitIndex || currentInput == TaxCalculator.returnIndex)
-                {
-
-                    checkedInput = currentInput;
-                }
-                else
-                {
-                    ShowWarning();
-                    GetUserInput(TypeOfUserInput.command);
-                }
-
+                TypeOfUserInputCommand();
             }
             return checkedInput;
         }
@@ -113,6 +49,80 @@ namespace Calculator_program
         private void ShowWarning()
         {
             Console.WriteLine("Помилка, неправильний ввiд! Спробуйте ще.");
+        }
+        private void TypeOfUserInputYear()
+        {
+            if (int.TryParse(currentInput, out int number))
+            {
+                checkedInput = currentInput;
+            }
+            else
+            {
+                ShowWarning();
+                GetUserInput(TypeOfUserInput.year);
+            }
+        }
+        private void TypeOfUserInputNumber()
+        {
+            if (int.TryParse(currentInput, out int number))
+            {
+                checkedInput = currentInput;
+            }
+            else
+            {
+                Console.Clear();
+            }
+        }
+        private void TypeOfUserInputMoney()
+        {
+            double number;
+            bool isLetter = currentInput.All(Char.IsLetter);
+            if (double.TryParse(currentInput, out number))
+            {
+                checkedInput = currentInput;
+            }
+            else if (isLetter == false && currentInput.Contains("."))
+            {
+                checkedInput = currentInput;
+            }
+            else
+            {
+                ShowWarning();
+                GetUserInput(TypeOfUserInput.money);
+            }
+        }
+        private void TypeOfUserInputCurrency()
+        {
+            if (int.TryParse(currentInput, out int number))
+            {
+                ShowWarning();
+                GetUserInput(TypeOfUserInput.currency);
+            }
+            else
+            {
+                if (currentInput == TaxCalculator.hryvniaIndex || currentInput == TaxCalculator.dollarIndex || currentInput == TaxCalculator.euroIndex)
+                {
+                    checkedInput = currentInput;
+                }
+                else
+                {
+                    ShowWarning();
+                    GetUserInput(TypeOfUserInput.currency);
+                }
+
+            }
+        }
+        private void TypeOfUserInputCommand()
+        {
+            if (currentInput == TaxCalculator.calcAgainIndex || currentInput == TaxCalculator.exitIndex || currentInput == TaxCalculator.returnIndex)
+            {
+                checkedInput = currentInput;
+            }
+            else
+            {
+                ShowWarning();
+                GetUserInput(TypeOfUserInput.command);
+            }
         }
     }
 }
