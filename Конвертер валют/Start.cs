75fb1c517d652f12c27f6b23674d13aa8c68b192
AntonAntipov29 <antipovhuman@gmail.com>
2022-09-22 21:49:09 +0300

    Added more methods to Start

--- Конвертер валют/Start.cs
+++ Конвертер валют/Start.cs
@@ -8,17 +8,40 @@ namespace Calculator_program
 {
     public class Start
     {
-        static void Main()
-        { 
-            
-            Console.WriteLine("Вiтеємо Вас в унiверсальному калькуляторi!");
-            Console.WriteLine("Натиснiть будь-яку кнопку, щоб продовжити.");
-            Console.ReadKey();
-            Console.Clear();
-            AgeControl ageControl = new AgeControl();
-            ageControl.Realization();
-            
 
+
+        static void Main()
+        {
+            Greeting();
+            AgeControler();
+            ShowMainMenu();
+
+            void Greeting()
+            {
+                Console.WriteLine("Вiтеємо Вас в унiверсальному калькуляторi!");
+                Console.WriteLine("Натиснiть будь-яку кнопку, щоб продовжити.");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            void AgeControler()
+            {
+                bool ageControlchecked;
+                AgeControl ageControl = new AgeControl();
+                ageControlchecked = AgeControl.ControlAsk();
+
+                if (ageControlchecked == false)
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                }
+            }
+            void ShowMainMenu()
+            {
+                ShowMainMenu showMainMenu = new ShowMainMenu();
+                showMainMenu.ShowMenu();
+            }
         }
     }
 }
