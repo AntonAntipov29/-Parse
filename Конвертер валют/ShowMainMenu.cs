8929e602e468f515543e0e0a99d244c98aa49887
AntonAntipov29 <antipovhuman@gmail.com>
2022-09-27 01:00:21 +0300

    Exit answer variant was added

--- Конвертер валют/ShowMainMenu.cs
+++ Конвертер валют/ShowMainMenu.cs
@@ -11,29 +11,27 @@ namespace Calculator_program
         public void ShowMenu()
         {
             string choiseInput;
-            int choiseInputInt;
             Console.WriteLine("Введiть номер калькулятора, який хочете використовувати:");
             Console.WriteLine("1. Простий калькулятор");
             Console.WriteLine("2. Калькулятор вiку");
             Console.WriteLine("3. Калькулятор податкiв");
-            Console.WriteLine("Щоб вийти введiть Exit");
 
             UserInput userInput = new UserInput();
-            choiseInput = userInput.GetUserInput(TypeOfUserInput.number, TypeOfUserInput.command, showWarning:true);
+            choiseInput = userInput.GetUserInput(TypeOfUserInput.number, TypeOfUserInput.command);
 
-            if (choiseInput == TaxCalculator.firstItem)
+            if (choiseInput == "1")
             {
                 Console.Clear();
                 SimpleCalculator simpleCalculator = new SimpleCalculator();
                 simpleCalculator.LaunchSimpleCalculator();
             }
-            else if (choiseInput == TaxCalculator.secondItem)
+            else if (choiseInput == "2")
             {
                 Console.Clear();
                 AgeCalculator ageCalculator = new AgeCalculator();
                 ageCalculator.LaunchAgeCalculator();
             }
-            else if (choiseInput == TaxCalculator.thirdItem)
+            else if (choiseInput == "3")
             {
                 Console.Clear();
                 TaxCalculator taxCalculator = new TaxCalculator();
@@ -42,7 +40,14 @@ namespace Calculator_program
             else if (choiseInput == TaxCalculator.exitIndex)
             {
                 Environment.Exit(0);
-            } 
+            }
+            else
+            {
+                Console.Clear();
+                UserInput userInputTwo = new UserInput();
+                userInputTwo.ShowWarning();
+                ShowMenu();
+            }
         }
     }
 }
