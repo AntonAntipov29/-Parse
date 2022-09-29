ca55fe877358fe61901a9cb902dcde0c5cc139b2
AntonAntipov29 <antipovhuman@gmail.com>
2022-09-29 13:06:12 +0300

    Simple Calculator added

--- Конвертер валют/SimpleCalculator.cs
+++ Конвертер валют/SimpleCalculator.cs
@@ -3,22 +3,155 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Calculator_program
 { 
     public class SimpleCalculator
     {
-        public void LaunchSimpleCalculator()
+        string firstNumber;
+        double firstNumberDouble;
+        string secondNumber;    
+        double secondNumberDouble;
+        string operation;
+        double result;
+        public const string additionIndex = "+";
+        public const string substractionIndex = "-";
+        public const string multiplicationIndex = "*";
+        public const string divisionIndex = "/"; 
+        public const string interestIndex = "%";
+        private const double fullPercentage = 100;
+        private const string formatMoney = "{0:N}";
+        private string finalAnswer;
+
+        UserInput userInput = new UserInput();
+
+        public void Show()
         {
-            Console.WriteLine("Данна функцiя буде додана згодом, очiкуйте на оновлення.");
-            Console.WriteLine("Щоб повернутись в попередньє меню введiть Return.");
-            Console.WriteLine("Щоб закрити додаток введiть Exit.");
-            string answer = Console.ReadLine();
-            if (answer == TaxCalculator.exitIndex)
+            GettingInput();
+            Calculation();
+            ShowResult();
+        }
+
+        private void GettingInput()
+        {
+            NumberFormatInfo formatComa = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ",",
+            };
+            NumberFormatInfo formatDot = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ".",
+            };
+
+            Console.WriteLine("Ви обрали простий калькулятор.");
+            Console.WriteLine("Введiть перше число");
+            firstNumber = userInput.GetUserInput(TypeOfUserInput.number);
+
+            if (firstNumber.Contains(","))
+            {
+                firstNumberDouble = Double.Parse(firstNumber, formatComa);              
+            }
+            else if (firstNumber.Contains("."))
+            {
+                firstNumberDouble = Double.Parse(firstNumber, formatDot);
+            }
+            else
+            {
+                firstNumberDouble = Double.Parse(firstNumber);
+            }
+
+            Console.Clear();
+            Console.WriteLine($"Ведiть потрiбну операцiю: |{additionIndex}| - додавання, |{substractionIndex}| - вiднiмання, |{multiplicationIndex}| - множення, |{divisionIndex}| -  дiлення,");
+            Console.WriteLine($"|{interestIndex}| - вiдсоткове спiввiдношення першого числа до другого ");
+            operation = userInput.GetUserInput(TypeOfUserInput.operation);
+            Console.Clear();
+            Console.WriteLine("Введiть друге число");
+            secondNumber = userInput.GetUserInput(TypeOfUserInput.number);
+
+            if (secondNumber.Contains(","))
+            {
+                secondNumberDouble = Double.Parse(secondNumber, formatComa);
+            }
+            else if (secondNumber.Contains("."))
+            {
+                secondNumberDouble = Double.Parse(secondNumber, formatDot);
+            }
+            else
+            {             
+                secondNumberDouble = Double.Parse(secondNumber);
+            } 
+
+            Console.Clear();
+            Console.WriteLine($"Ви ввели вираз: {firstNumber} {operation} {secondNumber}");
+            Console.WriteLine("Натиснiть ENTER, щоб продовжити.");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private void Calculation()
+        {
+            if (operation == additionIndex)
+	        {
+                result = firstNumberDouble + secondNumberDouble;
+	        }
+            else if (operation == substractionIndex)
+            {
+                result = firstNumberDouble - secondNumberDouble;
+            }
+            else if (operation == multiplicationIndex)
+            {
+                result = firstNumberDouble * secondNumberDouble;
+            }
+            else if (operation == divisionIndex)
+            {
+                result = firstNumberDouble / secondNumberDouble;
+            }
+            else if (operation == interestIndex)
+            {
+                result = (firstNumberDouble * fullPercentage) / secondNumberDouble;
+            }           
+        }       
+        
+        private void ShowResult()
+        {
+             if (operation == additionIndex)
+	        {
+                Console.Write("Результат вашого виразу: " + firstNumberDouble + " + " + secondNumberDouble + " = " + formatMoney, result);             
+	        }
+            else if (operation == substractionIndex)
+            {
+                Console.Write("Результат вашого виразу: " + firstNumberDouble + " - " + secondNumberDouble + " = " + formatMoney, result);
+            }
+            else if (operation == multiplicationIndex)
+            {
+                Console.Write("Результат вашого виразу: " + firstNumberDouble + " * " + secondNumberDouble + " = " + formatMoney, result);
+            }
+            else if (operation == divisionIndex)
+            {
+                Console.Write("Результат вашого виразу: " + firstNumberDouble + " / " + secondNumberDouble + " = " + formatMoney, result);
+            }
+            else if (operation == interestIndex)
+            {
+                Console.Write("Вiдсоткове спiввiдношення " + firstNumberDouble + " до " + secondNumberDouble + " складає " + formatMoney, result);
+                Console.WriteLine(" %");
+            }    
+                      
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("Щоб закрити програму напишiть Exit, щоб повернутись в головне меню напишiть Return.");
+            Console.WriteLine("Щоб рахувати знову напишiть Calculate again.");
+            finalAnswer = userInput.GetUserInput(TypeOfUserInput.command);
+            if (finalAnswer == TaxCalculator.exitIndex)
             {
                 Environment.Exit(0);
             }
-            else if (answer == TaxCalculator.returnIndex)
+            else if (finalAnswer == TaxCalculator.calcAgainIndex)
+            {
+                Console.Clear();
+                Show();
+            }
+            else if (finalAnswer == TaxCalculator.returnIndex)
             {
                 Console.Clear();
                 MainMenu mainMenu = new MainMenu();
@@ -27,10 +160,11 @@ namespace Calculator_program
             else
             {
                 Console.Clear();
-                UserInput userInputTwo = new UserInput();
-                userInputTwo.ShowWarning();
-                LaunchSimpleCalculator();
+                Console.WriteLine("Неправильний ввiд! Використовуйте Exit, Return або Calculate again.");
+                ShowResult();
+                userInput.GetUserInput(TypeOfUserInput.command);
             }
-        }
+        }   
     }
 }
+
