639829cfaf80da98f4a1d828134f8f58c4cceea1
AntonAntipov29 <antipovhuman@gmail.com>
2022-10-18 22:13:58 +0300

    Moved Commands to sepaate file

---
+++ Конвертер валют/Commands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_program
+{
+    public class Commands
+    {
+        public const string exitIndex = "Exit";
+        public const string calcAgainIndex = "Calculate again";
+        public const string returnIndex = "Return";
+    }
+}
