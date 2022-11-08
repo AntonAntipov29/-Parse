07b732a4823ca47920f3dd7ac8bf6bc9e9509bcc
AntonAntipov29 <antipovhuman@gmail.com>
2022-11-09 01:22:29 +0200

    Create Base Calculator View

---
+++ Конвертер валют/Model/BaseCalculator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_program
+{
+    public abstract class BaseCalculator : ICalculator
+    {
+        protected string name;
+        protected int id;
+        protected string finalAnswer;
+        protected InputController userInput = new InputController();
+        protected BaseCalculatorView baseCalculatorView = new BaseCalculatorView();
+
+        public BaseCalculator(string name, int id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+
+        public string Name { get { return name; } }
+
+        public int ID { get { return id; } }
+
+        public void Start()
+        {
+            ShowGreeting();
+            GettingInput();
+            Calculation();
+            AskFinalAnswer();
+        }
+
+        public void ShowGreeting()
+        {
+            baseCalculatorView.ShowGreeting(name);
+        }
+
+        public abstract void GettingInput();
+
+        public abstract void Calculation();
+
+        public void AskFinalAnswer()
+        {
+            baseCalculatorView.AskFinalAnswer();
+            finalAnswer = userInput.GetUserInput(TypeOfUserInput.command);
+            if (finalAnswer == Commands.exitIndex)
+            {
+                Environment.Exit(0);
+            }
+            else if (finalAnswer == Commands.calcAgainIndex)
+            {
+                Console.Clear();
+                Start();
+            }
+            else if (finalAnswer == Commands.returnIndex)
+            {
+                Console.Clear();
+                MainMenu mainMenu = new MainMenu();
+                mainMenu.ShowMenu();
+            }
+            else
+            {
+                Console.Clear();
+                userInput.ShowWarning();              
+                AskFinalAnswer();
+                userInput.GetUserInput(TypeOfUserInput.command);
+            }
+        }
+    }
+}
---
+++ Конвертер валют/View/BaseCalculatorView.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Calculator_program
+{
+    public class BaseCalculatorView : IVisible
+    {       
+        public void Show(string writeLineInput)
+        {
+            Console.WriteLine(writeLineInput);
+        }
+
+        public void ShowOnThirdLine(string writeLineInput)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine(writeLineInput);
+        }
+
+        public void Clear()
+        {
+            Console.Clear();
+        }
+        public void ShowSeparatorLine()
+        {
+            Console.WriteLine("______________________________________________________________");      
+        }
+
+        public void ShowGreeting(string name)
+        {
+            Console.WriteLine($"Ви обрали {name}!");
+        }
+
+        public void AskOfContinue()
+        {
+            Console.WriteLine("Натиснiть ENTER, щоб продовжити.");
+        }
+
+        public void AskFinalAnswer()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Щоб закрити програму напишiть Exit, щоб повернутись в головне меню напишiть Return.");
+            Console.WriteLine("Щоб рахувати знову напишiть Calculate again.");
+        }
+    }
+}
