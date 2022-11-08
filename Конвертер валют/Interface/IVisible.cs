4de567c7de62caa090682e2ec8e07dc0ca9e556b
AntonAntipov29 <antipovhuman@gmail.com>
2022-11-09 01:24:30 +0200

    Create IVisible

---
+++ Конвертер валют/Interface/IVisible.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_program
+{
+    internal interface IVisible
+    {
+        void Show(string writeLineInput);
+
+        void ShowOnThirdLine(string writeLineInput);
+
+        void Clear();
+
+        void ShowSeparatorLine();
+    }
+}
